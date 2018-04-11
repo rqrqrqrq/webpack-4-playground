f0cd8819a500671a8d3eb3335a96906206464935
rqrqrqrq <rqtdfh@gmail.com>
2018-04-11 20:21:12 +0300

    rewrite index for better hot reloading

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 
 import App from './App';
-import { RootProvider } from '@/modules/RootProvider';
+import { RootProvider } from './modules/RootProvider';
 
-render(
-  <RootProvider>
-    <App />
-  </RootProvider>,
-  document.getElementById('root'),
+const ROOT_EL_ID = 'root';
+
+const rootEl = document.getElementById(ROOT_EL_ID);
+
+if (!rootEl) {
+  throw new Error(`Cannot find element with id '#${ROOT_EL_ID}`);
+}
+
+const wrapInProvider = (Provider, AppComponent) => (
+  <Provider>
+    <AppComponent />
+  </Provider>
 );
+
+const render = vdom => ReactDOM.render(vdom, rootEl);
+
+render(wrapInProvider(RootProvider, App));
+
+if (module.hot) {
+  module.hot.accept(() => {
+    const newApp = require('./App');
+
+    const provider = require('./modules/RootProvider');
+
+    render(wrapInProvider(provider, newApp));
+  });
+}
